34505f30ac227956840915575f37d36030242a17
naresh kumar <kumarnaresh32621@gmail.com>
2024-01-15 06:09:21 +0530

    deleted exe

---
+++ basic/vscode/basic2.js
@@ -0,0 +1,14 @@
+console.log("hello naresh")
+let x,y,z;
+x=2;
+y=3
+z=x+y
+console.log("sum=",z)
+function sum(x,y){
+    z=x+y
+    //console.log
+    let a=56
+    return a
+}
+
+console.log(sum(9,8))
---
+++ html/new.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+function myfunction(){
+    console.log("hello");
+
+}
+console.log("hi");
+myfunction();
+let cars=[67,78,89,90,89,78,59,34];
+const arrow=(a,b)=>{
+    console.log(a+b);
+    return a+b;
+}
+arrow(7,8);
+
+
